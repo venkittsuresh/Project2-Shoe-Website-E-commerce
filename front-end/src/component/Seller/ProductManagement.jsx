69d721de4c7a83e3fc1d972bc6a026cb8b48b308
venkitt suresh <venkittsuresh@gmail.com>
2024-08-07 23:17:29 +0530

    Product added

--- front-end/src/component/Seller/ProductManagement.jsx
+++ front-end/src/component/Seller/ProductManagement.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import './ProductManagement.css';
-import ProductForm from './Components/ProductForm/ProductForm';
-import Navbar from '../common/Navbar/Navbar';
+import React from "react";
+import "./ProductManagement.css";
+import ProductForm from "./Components/ProductForm/ProductForm";
+import Navbar from "../common/Navbar/Navbar";
+import ProductList from "./Components/ProductList";
 
 const ProductManagement = () => {
-
-  return(
-    <>
-    <Navbar/>
-    <h1>Product pages</h1>
-    </>
-  )
- 
+  return (
+    <div className="product-main">
+      <Navbar />
+      <div className="Product-button">
+      <button className="button-product">Add Product</button>
+      </div>
+      <div className="product-list">
+        <ProductList/>
+      </div>
+    </div>
+  );
 };
 
 export default ProductManagement;
